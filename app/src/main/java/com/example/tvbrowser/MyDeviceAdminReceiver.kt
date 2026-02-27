dd9da6fd59fd4ff61fc4c31c3ce730bb1c4c989b
kriztpow <45737457+kriztpow@users.noreply.github.com>
2026-02-27 16:16:40 -0400

    Create MyDeviceAdminReceiver.kt

---
+++ app/src/main/java/com/example/tvbrowser/MyDeviceAdminReceiver.kt
@@ -0,0 +1,9 @@
+package com.example.tvbrowser
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyDeviceAdminReceiver : DeviceAdminReceiver() {
+    // Esta clase permite que la app reciba privilegios de sistema
+}
